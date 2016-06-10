5ef6bce4730486b9d289ad28e3a84c5a428bdc0b
Ali Sharif <alisharif@outlook.de>
2016-06-10 12:25:00 +0200

    Erstes Commit C-Projekt

---
+++ Hello-World/Hello-World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt! Das ist die erste Zeile für das GIT-Projekt.");
+            Console.ReadKey();
+        }
+    }
+}
